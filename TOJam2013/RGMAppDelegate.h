d9b2a04aa25eb96046dec3f20649d6a507037140
Ryder Mackay <ryder.mackay@gmail.com>
2013-05-03 02:41:44 -0400

    Committing to commit

---
+++ TOJam2013/RGMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RGMAppDelegate.h
+//  TOJam2013
+//
+//  Created by Ryder Mackay on 2013-05-03.
+//  Copyright (c) 2013 Ryder Mackay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RGMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TOJam2013/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TOJam2013
+//
+//  Created by Ryder Mackay on 2013-05-03.
+//  Copyright (c) 2013 Ryder Mackay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RGMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RGMAppDelegate class]));
+    }
+}
